23d253a160e9aabf1e63ebbf60c5d46b97d1158c
Louis Alonzo <Louis.A@live.com>
2019-10-26 13:40:29 -0700

    completed third AJAX call and verified it can complete the call. Dynamically created a list of previous searched cities and rendered them as buttons to the page

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ searchButton.on("click", function (event) {
     event.preventDefault();
     var cityInput = $("#cityTyped").val();
     renderCurrentWeather(cityInput);
+    $("#cityHistory").append("<button>" + cityInput + "</button> <br>");
 });
 
 // console.log(cityInput);
@@ -20,8 +21,10 @@ searchButton.on("click", function (event) {
 function renderCurrentWeather(cityInput) {
     // gets values for URL based on count and type
     var cityName = cityInput;
-    var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=96572f2661bb3c833bf8894b537b2366";
+    var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=96572f2661bb3c833bf8894b537b2366&units=imperial";
     var currentWeather = $(".currentWeather");
+    // variable for third AJAX call
+    var fiveDayForecast = $(".fiveDayDisplay");
 
     // console log my API URL to check that variables have a value assigned
     console.log(openWeatherURL);
@@ -34,7 +37,7 @@ function renderCurrentWeather(cityInput) {
           console.log(response);
         //   currentWeather.append("<p>City Name:" + response.name + "</p>");
         console.log(response.name);
-        currentWeather.text("City Name:" + response.name);
+        $("#cityName").text("City Name: " + response.name);
         currentWeather.text("Weather Description: "+ response.weather[0].description); 
         currentWeather.append("<p>Temperature: " + response.main.temp + "</p>");
         currentWeather.append("<p>Humidity: " + response.main.humidity + "</p>");
@@ -44,7 +47,7 @@ function renderCurrentWeather(cityInput) {
         var lat = response.coord.lat;
         var lon = response.coord.lon;
         // URL for second AJAX call to obtain UV data
-        var dataUvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=96572f2661bb3c833bf8894b537b2366&lat=" + lat + "&lon=" + lon;
+        var dataUvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=96572f2661bb3c833bf8894b537b2366&lat=" + lat + "&lon=" + lon + "&units=imperial";
 
         // console log the variables for the second AJAX call to view the results
         console.log(lat);
@@ -61,7 +64,7 @@ function renderCurrentWeather(cityInput) {
          });
         
         // additonal variable for third AJAX call
-        var fiveDayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon;
+        var fiveDayForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=96572f2661bb3c833bf8894b537b2366&units=imperial";
 
         console.log(fiveDayForecastURL);
 
@@ -70,6 +73,15 @@ function renderCurrentWeather(cityInput) {
             method: "GET"
         }).then(function(responseThree){
             console.log(responseThree);
+
+            fiveDayData = responseThree.list
+
+            for (i = 0; i<fiveDayData.length; i += 8) {
+            fiveDayForecast.append("<h5>" + responseThree.list[i].dt_txt + "</h5>");
+            // fiveDayForecast.append("<p> Humidity: "  + responseThree.list[0].main.humidity) + "</p>";
+            fiveDayForecast.append("<p> Temperature: "  + responseThree.list[i].main.temp) + "</p>";
+            fiveDayForecast.append("<p> Humidity: "  + responseThree.list[i].main.humidity) + "%</p>";
+        }
         })
 
 

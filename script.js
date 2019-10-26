005422bb5cb2b425c20f027ebae9aa70aa1e6074
Louis Alonzo <Louis.A@live.com>
2019-10-25 18:57:31 -0700

    revised variables to make a valid API call. Appended response data to html page

--- script.js
+++ script.js
@@ -2,22 +2,24 @@
 
 
 
-var cityInput = $("#cityTyped").value();
+// var cityInput = $("#cityTyped").val();
 var searchButton = $("#citySearch");
 
 
 searchButton.on("click", function (event) {
     event.preventDefault();
-    renderCurrentWeather();
+    var cityInput = $("#cityTyped").val();
+    renderCurrentWeather(cityInput);
 });
 
-console.log(cityInput);
-console.log(searchButton);
+// console.log(cityInput);
+// console.log(searchButton);
 
-function renderCurrentWeather() {
+function renderCurrentWeather(cityInput) {
     // gets values for URL based on count and type
     var cityName = cityInput
     var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=96572f2661bb3c833bf8894b537b2366";
+    var currentWeather = $(".currentWeather");
 
     console.log(openWeatherURL);
 
@@ -25,9 +27,15 @@ function renderCurrentWeather() {
         url: openWeatherURL,
         method: "GET"
       }).then(function(response) {
-        $(".currentWeather").text(response.weather);
-      });
+          console.log(response);
+        //   currentWeather.append("<p>City Name:" + response.name + "</p>");
+        console.log(response.name);
+        currentWeather.text("Weather Description: "+ response.weather[0].description); 
+        currentWeather.append("<p>Temperature: " + response.main.temp + "</p>");
+        currentWeather.append("<p>Humidity: " + response.main.humidity + "</p>");
+        currentWeather.append("<p>Wind Speed: " + response.wind.speed + "</p>");
+      }).catch(function(err){console.log(err)});
 
     
 };
-renderCurrentWeather();
+
